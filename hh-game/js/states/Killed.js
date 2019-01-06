cb44adfc66b11f1b84c205ab27e019dc346aafa2
vishal_7 <vishal_7@hotmail.co.uk>
2019-01-06 17:48:14 +0000

    fixed the issue with the "space to restart" message when you die or win

--- hh-game/js/states/Killed.js
+++ hh-game/js/states/Killed.js
@@ -4,8 +4,7 @@ class Killed extends Phaser.State {
         this.in_rooms = in_rooms;
         this.game.add.text(this.game.world.centerX, this.game.world.centerY-100, "Game Over!", {font: "20px Arial"});
         this.game.add.text(this.game.world.centerX, this.game.world.centerY, "You fell into the abyss and died :(", {font: "20px Arial"});
-        this.game.add.text(this.game.world.centerX, this.game.world.centerY+100, "Press the Spacebar to Restart", {font: "20px Arial"});
-        this.finishedLoadingText = this.game.add.text(this.game.world.centerX+200, this.game.world.centerY+200, "Press Space to restart", { font: "25px Arial", fill: "#ffffff", align: "center" });
+        this.finishedLoadingText = this.game.add.text(this.game.world.centerX-100, this.game.world.centerY+200, "Press Space to restart", { font: "25px Arial", fill: "#ffffff", align: "center" });
         this.restartButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         this.game.input.keyboard.removeKeyCapture(Phaser.Keyboard.SPACEBAR);
     }
